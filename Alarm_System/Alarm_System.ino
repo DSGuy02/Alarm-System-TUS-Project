82b54f329b10b9a29715818481e6e7e0a13063be
Oladeji <oladejisanyaolu@gmail.com>
2025-03-16 19:27:45 +0000

    project is essentially finished; the oled components i have doesn't work ;P

--- Alarm_System/Alarm_System.ino
+++ Alarm_System/Alarm_System.ino
@@ -18,6 +18,8 @@
 // Define the Screen Width, Height - in pixels
 #define OLED_SCREEN_WIDTH 128
 #define OLED_SCREEN_HEIGHT 64
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_SCREEN_ADDRESS 0x3C
 
 // Pin Numbers
 const int RED_LED = 13;
@@ -32,7 +34,7 @@ const int ENTRY_EXIT_ULTRASONIC_ECHO = 6;
 const int ZONE_ONE_PIR = 48;
 #define ZONE_ONE_SOUND A2
 
-const int ZONE_TWO_PIR = 46;
+const int ZONE_TWO_IR_DIGITAL = 46;
 #define ZONE_TWO_TEMP A0
 
 // Time constants
@@ -53,14 +55,14 @@ long entry_exit_duration;
 float entry_exit_distance;
 
 // Variables for my_delay()
-int period = 1000;
+//int period = 1000;
 unsigned long time_now = 0;
 
 // Defining the Password function
 Password password = Password("1234");
 
 // Defining the OLED function (SSD1306 connected to I2C)
-Adafruit_SSD1306 oled(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 oled(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire, OLED_SCREEN_ADDRESS);
 
 /**************
 * Defining the Keypad Function
@@ -89,6 +91,7 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup() {
   Serial.begin(9600);
 
+  //I2CScanner();
   // Initialize digital pin LED_BUILTIN as an output.
   //pinMode(LED_BUILTIN, OUTPUT);
 
@@ -110,7 +113,7 @@ void setup() {
   pinMode(ZONE_ONE_SOUND, INPUT);
 
   // Setup for Zone 2
-  pinMode(ZONE_TWO_PIR, INPUT);
+  pinMode(ZONE_TWO_IR_DIGITAL, INPUT);
   pinMode(ZONE_TWO_TEMP, INPUT);
 
   // Have the keypad listen for input
@@ -127,11 +130,14 @@ void setup() {
 void loop() {
   keypad.getKey();
   toggleAlarm(); // Check if the alarm has been enabled
-
+  displayOLEDText("Alarm Disabled");
 
   while(alarmArmed == true) { // The state is set to ON
     keypad.getKey(); // Just in case the home-owner wishes to disarm the alarm
     Serial.println("ALARM_ENABLED"); // For Processing
+
+    // Clear the OLED
+    oled.clearDisplay();
 
     // Check the zones
     read_entry_exit();
@@ -139,7 +145,7 @@ void loop() {
     read_zone_2();
 
     if (EntryExitState == 1) { // Entry/Exit zone has been breached
-      Serial.println("Entry/Exit zone accessed. Initating timer...."); // TODO: Use for OLED
+      displayOLEDText("Entry/Exit zone accessed."); // TODO: Use for OLED
       countdown(10);
       // Serial.println("Entry Exit zone breeched"); // TODO: Use for OLED
       keypad.getKey();
@@ -147,6 +153,8 @@ void loop() {
       while(alarmArmed == true) { // The alarm has been triggered
         keypad.getKey();
         flash();
+
+        displayOLEDTextPositon("Entry/Exit Breached!", 10, 10);
         Serial.println("ENTRY_EXIT_BREACH"); // For Processing
       }
     }
@@ -160,7 +168,8 @@ void loop() {
       while(alarmArmed == true) { // The alarm has been triggered by Zone 1
         keypad.getKey();
         flash();
-        // Serial.println("Zone 1 Breached"); // TODO: Use for OLED
+
+        displayOLEDTextPositon("Zone 1 Breached!", 10, 10); // Serial.println("Zone 1 Breached"); // TODO: Use for OLED
         Serial.println("ZONE_1_BREACH"); // For Processing
       }
     }
@@ -172,7 +181,8 @@ void loop() {
       while(alarmArmed == true) { // The alarm has been triggered by Zone 2
         keypad.getKey();
         flash();
-        // Serial.println("Zone 1 Breached"); // TODO: Use for OLED
+
+        displayOLEDTextPositon("Zone 2 Breached!", 10, 10); // OLED Display
         Serial.println("ZONE_2_BREACH");
       }
     }
@@ -190,21 +200,20 @@ void flash() {
 
   changeRedLED(true); // digitalWrite(RED_LED, HIGH); // digitalWrite(LED_BUILTIN, HIGH); // turn the RED LED on (HIGH is the voltage level)
   playBuzzer(BUZZER, 1500, 200);
-  my_delay();//delay(1000);
+  custom_delay(200);//delay(1000);
   
   changeRedLED(false); // digitalWrite(RED_LED, LOW); // digitalWrite(LED_BUILTIN, LOW); // turn the RED LED off by making the voltage LOW
   playBuzzer(BUZZER, 500, 200);
-  my_delay();//delay(1000);
+  custom_delay(200);//delay(1000);
 }
 
-void my_delay() {
+void custom_delay(int period) {
   time_now = millis();
 
   while(millis() < time_now + period) { // wait approx. (period) ms
     keypad.getKey();
   }
 }
-
 
 /*
   Play Sound
@@ -226,9 +235,10 @@ void stopBuzzer(int buzzer) {
 // Countdown function
 void countdown(int timer) {
   for (int z = timer; z > 0; z--) { // Start from 10, down to 0
-    Serial.println("Alarm will arm in ");
-    Serial.print(z - 1); Serial.println(" seconds");
-    my_delay();//delay(1000); // Wait for one second before looping again
+    //Serial.println("Alarm will arm in ");
+    displayOLEDTextPositon("Alarm trigger in " + String(z - 1) + " seconds", 10, 10);
+    //Serial.print(z - 1); Serial.println(" seconds");
+    custom_delay(1000);//delay(1000); // Wait for one second before looping again
 
     if (alarmArmed == false) // Stop the countdown
       break;
@@ -239,9 +249,12 @@ void countdown(int timer) {
 void checkPassword() {
   if (password.evaluate()) {
     Serial.println("KEYPAD_SUCCESS"); // For Processing
+    displayOLEDTextPositon("Password Success!", 10, 30);
     alarmArmed = false;
   } else {
     Serial.println("KEYPAD_FAIL"); // For Processing
+    displayOLEDTextPositon("Invalid Password!", 10, 30);
+    oled.clearDisplay(); // Clear the OLED;
   }
 
   password.reset();
@@ -277,13 +290,30 @@ void initOLED() {
   }
 
   delay(2000); // wait for init
+}
+
+// Clears the display and shows text at (0, 10)
+void displayOLEDText(String text) {
   oled.clearDisplay(); // clear display
 
   oled.setTextSize(1);
   oled.setTextColor(WHITE);
   oled.setCursor(0, 10);
-  oled.println("Hello World!");
+  oled.println(text);
   oled.display();
+}
+
+// Does not clear display and adds text to a selected coord
+void displayOLEDTextPositon(String text, int x, int y) {
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.setCursor(x, y);
+  oled.println(text);
+  oled.display();
+}
+
+void clearOLED() {
+  oled.clearDisplay();
 }
 
 // Check if the Alarm has been turned on
@@ -355,21 +385,22 @@ void read_zone_1() { // Zone 1 is operated by a PIR and a sound detector
     ZoneOneState = 1;
   } else if (soundValue >= 70) {
     Serial.println("ZONE_1_SOUND_DETECT");
+    ZoneOneState = 1;
   } else {
     ZoneOneState = 0;
   }
 }
 
 void read_zone_2() { // Zone 2 
-  int pirTwoState = digitalRead(ZONE_TWO_PIR);
+  int irTwoState = digitalRead(ZONE_TWO_IR_DIGITAL);
   int tempValue = analogRead(ZONE_TWO_TEMP);
 
   //Serial.print("Temp. Value: ");
   //Serial.println(tempValue);
 
-  if (pirTwoState == 1) {
-    //Serial.println("ZONE_2_PIR_DETECT");
-    //ZoneTwoState = 1;
+  if (irTwoState == 0) {
+    Serial.println("ZONE_2_IR_DETECT");
+    ZoneTwoState = 1;
   } else if (tempValue >= 1030) {
     Serial.println("ZONE_2_TEMP_DETECT");
     ZoneTwoState = 1;
@@ -380,21 +411,6 @@ void read_zone_2() { // Zone 2
 **************/
 
 
-// Serial Print for Processing
-void sendData() {
-  Serial.println("ALARM_ENABLED");
-  Serial.println("ALARM_DISABLED");
-
-  Serial.println("ENTRY_EXIT_BREACH");
-  Serial.println("ENTRY_EXIT_SLEEP");
-
-  Serial.println("ZONE_1_BREACH");
-  Serial.println("ZONE_1_SLEEP");
-
-  Serial.println("ZONE_2_BREACH");
-  Serial.println("ZONE_2_SLEEP");
-}
-
 /**************
 * Event handler
 ***************/
@@ -404,6 +420,7 @@ void keypadEvent(KeypadEvent keyEvent) {
       Serial.print("Pressed: ");
       Serial.print(keyEvent);
       Serial.println();
+      displayOLEDTextPositon("Key: " + String(keyEvent), 10, 20);
 
       switch(keyEvent) {
         case '*':
@@ -421,3 +438,37 @@ void keypadEvent(KeypadEvent keyEvent) {
 /**************
 * End of Event handler
 ***************/
+
+
+// I2C Scanner (to find the OLED)
+void I2CScanner() {
+  Serial.println();
+  Serial.println ("I2C scanner. Scanning ...");
+
+  byte count = 0;
+
+  pinMode(12, OUTPUT);
+
+  digitalWrite(12, HIGH);
+  Wire.begin();
+
+  for (byte i = 1; i < 120; i++) {
+    Wire.beginTransmission(i);
+
+    if (Wire.endTransmission() == 0) {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);
+    } 
+  }
+
+  Serial.println("Done.");
+  Serial.print("Found ");
+  Serial.print(count, DEC);
+  Serial.println(" device(s).");
+
+}
